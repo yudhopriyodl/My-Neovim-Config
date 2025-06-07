be20ae5e21722cc54be1736784a46af3dc0f5d84
yudhopriyodl <95563554+yudhopriyodl@users.noreply.github.com>
2025-06-07 21:51:26 +0700

    Add files via upload

---
+++ lua/plugins/gemini.lua
@@ -0,0 +1,12 @@
+return {
+  "kiddos/gemini.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+}
